4c166a04ead8c6d84fe88ab513322e45e2899fc8
iperson98 <amade002@ucr.edu>
2017-07-18 13:08:39 -0700

    Modified Activites & Added Colors

--- app/src/main/java/com/example/mapdemo/PhotoTakeActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.mapdemo;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class PhotoTakeActivity extends AppCompatActivity {
-}
---
+++ app/src/main/java/com/example/mapdemo/PhotoUploadActivity.java
@@ -0,0 +1,8 @@
+package com.example.mapdemo;
+
+/**
+ * Created by amade002 on 7/18/17.
+ */
+
+public class PhotoUploadActivity {
+}
