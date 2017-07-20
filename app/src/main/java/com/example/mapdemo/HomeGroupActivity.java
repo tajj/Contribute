46f49ae8f82307211d4e54835447f82d76088990
Taylor Jean-Jacques <tjeanjacques@fb.com>
2017-07-20 14:03:27 -0700

    facebook sdk, login, parse, ALOT of code

--- app/src/main/java/com/example/mapdemo/HomeGroupActivity.java
+++ app/src/main/java/com/example/mapdemo/HomeGroupActivity.java
@@ -1,4 +1,65 @@
 package com.example.mapdemo;
 
-public class HomeGroupActivity {}
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+public class HomeGroupActivity extends AppCompatActivity {
+    Intent i;
+    Button btnAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homegroup_activity);
+
+        //ParseUser currentUser = ParseUser.getCurrentUser();
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        i=new Intent(HomeGroupActivity.this, SelectGroupMembers.class);
+
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                try{
+                    ParseObject newGrp=new ParseObject("Groups");
+                    newGrp.save();
+
+
+                    i.putExtra("grpId", newGrp.getObjectId());
+
+                    //Log.d("Grp id", newGrp.getObjectId());
+                    ParseObject addUsr=new ParseObject("UserConnections");
+                    addUsr.put("userId", ParseUser.getCurrentUser().getEmail());
+                    addUsr.put("userGroup", newGrp.getObjectId());
+                    addUsr.save();
+                   // Log.d("Credentials: ", ParseUser.getCurrentUser().getEmail()+" "+newGrp.getObjectId());
+                    Toast.makeText(getApplicationContext(), "Success! Group Created.", Toast.LENGTH_LONG).show();
+                }catch (ParseException e){
+                    Log.e("Error Creating Group: ", e.getMessage());
+                }
+
+
+
+
+                startActivity(i);
+                finish();
+
+            }
+
+
+        });
+
+    }
+
+}
 
---
+++ app/src/main/java/com/example/mapdemo/LoginActivity.java
@@ -0,0 +1,323 @@
+package com.example.mapdemo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    CircleImageView mProfileImage;
+    Button mBtnFb;
+    Button btnProceed;
+    TextView mUsername, mEmailID;
+    Profile mFbProfile;
+    ParseUser parseUser;
+    int count =0;
+
+    String name = null, email = null;
+
+    public static final List<String> mPermissions = new ArrayList<String>() {{
+        add("public_profile");
+        add("email");
+    }};
+
+    //use butterknife
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mBtnFb = (Button) findViewById(R.id.btn_fb_login);
+        mProfileImage = (CircleImageView) findViewById(R.id.profile_image);
+
+        btnProceed = (Button) findViewById(R.id.btnProceed);
+
+        mUsername = (TextView) findViewById(R.id.txt_name);
+        mEmailID = (TextView) findViewById(R.id.txt_email);
+
+        mFbProfile = Profile.getCurrentProfile();
+
+//  Use this to output your Facebook Key Hash to Logs
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.example.demo",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException | NoSuchAlgorithmException e) {
+//
+//        }
+
+        mBtnFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ParseFacebookUtils.logInWithReadPermissionsInBackground(LoginActivity.this, mPermissions, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException err) {
+
+
+                        if (user == null) {
+                            Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+                        } else if (user.isNew()) {
+                            Log.d("MyApp", "User signed up and logged in through Facebook!");
+                            getUserDetailsFromFB();
+                            count++;
+                            // displayHomeGroupActivity();
+
+                        } else {
+                            Log.d("MyApp", "User logged in through Facebook!");
+                            getUserDetailsFromParse();
+                             count++;
+
+                        }
+
+
+                    }
+                });
+
+//                if (!ParseFacebookUtils.isLinked(user)) {
+//                    ParseFacebookUtils.linkWithReadPermissionsInBackground(user, this, permissions, new SaveCallback() {
+//                        @Override
+//                        public void done(ParseException ex) {
+//                            if (ParseFacebookUtils.isLinked(user)) {
+//                                Log.d("MyApp", "Woohoo, user logged in with Facebook!");
+//                            }
+//                        }
+//                    });
+//                }
+
+            }
+        });
+
+        btnProceed.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if (count !=0) {
+                    Intent i = new Intent(LoginActivity.this, HomeGroupActivity.class);
+                    startActivity(i);
+                }
+                    finish();
+
+            }
+
+
+        });
+
+    }
+
+    private void displayHomeGroupActivity() {
+        Intent i = new Intent(LoginActivity.this, HomeGroupActivity.class);
+        startActivity(i);
+    }
+
+
+    private void saveNewUser() {
+        parseUser = ParseUser.getCurrentUser();
+        parseUser.setUsername(name);
+        parseUser.setEmail(email);
+
+
+//        Saving profile photo as a ParseFile
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        Bitmap bitmap = ((BitmapDrawable) mProfileImage.getDrawable()).getBitmap();
+        if (bitmap != null) {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+            byte[] data = stream.toByteArray();
+            String thumbName = parseUser.getUsername().replaceAll("\\s+", "");
+            final ParseFile parseFile = new ParseFile(thumbName + "_thumb.jpg", data);
+
+            parseFile.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    parseUser.put("profileThumb", parseFile);
+
+                    //Finally save all the user details
+                    parseUser.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            Toast.makeText(LoginActivity.this, "New user:" + name + " Signed up", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }
+            });
+        }
+
+    }
+
+    private void getUserDetailsFromFB() {
+
+        // Suggested by https://disqus.com/by/dominiquecanlas/
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "email,name,picture");
+
+
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me",
+                parameters,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+            /* handle the result */
+                        try {
+
+                            Log.d("Response", response.getRawResponse());
+
+                            email = response.getJSONObject().getString("email");
+                            mEmailID.setText(email);
+
+                            name = response.getJSONObject().getString("name");
+                            mUsername.setText(name);
+
+                            JSONObject picture = response.getJSONObject().getJSONObject("picture");
+                            JSONObject data = picture.getJSONObject("data");
+
+                            //  Returns a 50x50 profile picture
+                            String pictureUrl = data.getString("url");
+
+                            Log.d("Profile pic", "url: " + pictureUrl);
+
+                            new ProfilePhotoAsync(pictureUrl).execute();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).executeAsync();
+
+
+        //displayHomeGroupActivity();
+    }
+
+    private void getUserDetailsFromParse() {
+        parseUser = ParseUser.getCurrentUser();
+
+//Fetch profile photo
+        try {
+            ParseFile parseFile = parseUser.getParseFile("profileThumb");
+            byte[] data = parseFile.getData();
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            mProfileImage.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mEmailID.setText(parseUser.getEmail());
+        mUsername.setText(parseUser.getUsername());
+
+        Toast.makeText(LoginActivity.this, "Welcome back " + mUsername.getText().toString(), Toast.LENGTH_SHORT).show();
+
+        // displayHomeGroupActivity();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    class ProfilePhotoAsync extends AsyncTask<String, String, String> {
+        public Bitmap bitmap;
+        String url;
+
+        public ProfilePhotoAsync(String url) {
+            this.url = url;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            // Fetching data from URI and storing in bitmap
+            bitmap = DownloadImageBitmap(url);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            mProfileImage.setImageBitmap(bitmap);
+
+            saveNewUser();
+        }
+    }
+
+    public static Bitmap DownloadImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+            Log.e("IMAGE", "Error getting bitmap", e);
+        }
+        return bm;
+
+
+    }
+
+}
+    //proceed button SOS
+
